8f8dc612788314bad429caaae8c2955f67bb8c3d
Isaac Okoroafor <isaacokoroafor@Isaacs-MacBook-Pro.local>
2022-05-25 20:13:40 +0100

    This is a new commit for what I originally planned to be amended

--- components/Header.js
+++ components/Header.js
@@ -3,28 +3,65 @@ import React from 'react';
 import classes from './Header.module.scss';
 import {BiMenuAltRight} from 'react-icons/bi';
 import {AiOutlineClose} from 'react-icons/ai';
+import {useState} from "react";
+import { useEffect } from "react";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(true);
+    const [size, setSize] = useState({
+      width: undefined,
+      height: undefined,
+  });
+
+  useEffect(() => {
+      const handleResize = () => {
+          setSize({
+              width: window.innerWidth,
+              height: window.innerHeight,
+          });
+      };
+      window.addEventListener("resize", handleResize);
+
+      return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+      if (size.width > 768 && menuOpen) {
+          setMenuOpen(false);
+      }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
   return (
     <header className={classes.header}> 
       <div className={classes.header__content}>
-            <h2 className={classes.header__content__logo}>header</h2>
-        <nav className={classes.header__content__nav}>
-          <ul>
-            <li>
-              <a href="#">PageOne</a>
-            </li>
-            <li>
-              <a href="#">PageTwo</a>
-            </li>
-            <li>
-              <a href="#">PageThree</a>
-            </li>
-          </ul>
-          <button>CTA Page</button>
+        <h2 className={classes.header__content__logo}>NZA</h2>
+
+        <nav 
+            className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ""}`}
+        >
+            <ul>
+                <li>
+                  <a href="#">PageOne</a>
+                </li>
+                <li>
+                  <a href="#">PageTwo</a>
+                </li>
+                <li>
+                  <a href="#">PageThree</a>
+                </li>
+            </ul>
+            <button>CTA Page</button>
         </nav>
         <div className={classes.header__content__toggle}>
-            <BiMenuAltRight />
+            {!menuOpen ? ( 
+                <BiMenuAltRight onClick={menuToggleHandler} /> 
+            ) : ( 
+                <AiOutlineClose onClick={menuToggleHandler} />
+            )}
         </div>
       </div>
     </header>
